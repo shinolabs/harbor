86846607c6a539a22cbed0fa41ad3e4b7e650ca8
naomiEve <0xlunaric@gmail.com>
2025-01-26 16:16:20 +0100

    feat: limit blobs to 50MiB.

--- lib/harbor/pds.ex
+++ lib/harbor/pds.ex
@@ -1,11 +1,35 @@
 defmodule Harbor.Pds do
-	
+
+	@max_file_size 52428800
+
 	def get_blob(pds, did, cid) do
-		case HTTPoison.get("#{pds}/xrpc/com.atproto.sync.getBlob?did=#{did}&cid=#{cid}") do
-			{ :ok, %{ status_code: 200, body: body} } ->
-				{ :ok, body }
+		url = "#{pds}/xrpc/com.atproto.sync.getBlob?did=#{did}&cid=#{cid}"
+		with { :ok, size } <- get_content_length_header(url),
+			size <= @max_file_size do
+			case HTTPoison.get(url) do
+				{ :ok, %{ status_code: 200, body: body} } ->
+					{ :ok, body }
+				_ ->
+					{ :error, "Could not get blob" }
+			end
+		else
+			{ :error, err } ->
+				{ :error, err }
+		end
+
+	end
+
+	def get_content_length_header(url) do
+		case HTTPoison.head(url) do
+			{ :ok, %{ status_code: 200, headers: headers }} ->
+				{ _, size } = headers |>
+					Enum.find(fn { key, _ } ->
+						String.match?(key, ~r/content-length/i)
+					end)
+
+				{ :ok, Integer.parse(size) }
 			_ ->
-				{ :error, "Could not get blob" }
+				{ :error, "PDS does not resport the size of the blob." }
 		end
 	end
 
